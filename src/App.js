0f3198fe469ddbe0ad1b28a0538ada0c2d23e565
Julia Ortheden <j.ortheden@hotmail.com>
2018-12-02 12:16:01 +0100

    Added source code for the website

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu />
+        <div>
+          <h1 style={headerStyle}>Hey there, 
+            I'm Julia</h1>
+          <h2 style={textStyle}> A Swedish software engineering student. </h2>
+        </div>
       </div>
     );
   }
 }
 
+const headerStyle = {
+  color: '#FFF',
+  fontSize: '60px',
+  fontWeight: 'normal',
+  //fontFamily: 'Helvetica'
+}
+const textStyle = {
+  color: '#FFF',
+  fontSize: '20px',
+  fontWeight: 'normal',
+  //fontStyle: 'italic',
+}
+const linkStyle = {
+  color: '#FFF',
+  fontSize: '20px',
+  fontWeight: 'normal',
+  margin: "0",
+  top: '0', 
+  //fontStyle: 'italic',
+}
 export default App;
