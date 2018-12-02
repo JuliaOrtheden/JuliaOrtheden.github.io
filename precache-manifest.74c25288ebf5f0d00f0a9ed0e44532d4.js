02bcb60627798c57e2e5d91cbe8bb04c534948e5
Julia Ortheden <j.ortheden@hotmail.com>
2018-12-02 12:26:05 +0100

    Updates

--- precache-manifest.74c25288ebf5f0d00f0a9ed0e44532d4.js
+++ precache-manifest.74c25288ebf5f0d00f0a9ed0e44532d4.js
@@ -28,6 +28,10 @@ self.__precacheManifest = [
     "url": "/my-react-gh-pages/static/media/outline-icons.ef60a4f6.woff"
   },
   {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/my-react-gh-pages/static/media/flags.9c74e172.png"
+  },
+  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/my-react-gh-pages/static/media/outline-icons.701ae6ab.eot"
   },
@@ -38,10 +42,6 @@ self.__precacheManifest = [
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/my-react-gh-pages/static/media/icons.0ab54153.woff2"
-  },
-  {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/my-react-gh-pages/static/media/flags.9c74e172.png"
   },
   {
     "revision": "5b3e720be48c3cae75e0",
@@ -64,16 +64,16 @@ self.__precacheManifest = [
     "url": "/my-react-gh-pages/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/my-react-gh-pages/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/my-react-gh-pages/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/my-react-gh-pages/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/my-react-gh-pages/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
